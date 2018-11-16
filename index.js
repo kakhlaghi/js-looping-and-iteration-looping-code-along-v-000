8a394d4d6abb93f4e02e020228c52907ded9f97a
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 16:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,3 @@ function printBadges(badges) {
 }
 
 
-function tailsNeverFails(){
-  let res;
-  let count;
-  while (res != 'tails'){
-    if (Math.random() >= 0.5 ){
-      count ++ 1
-    } else {
-      res = 'heads'
-      console.log( `You got Tails ${count} many times!`)
-    }
-  }
-}
