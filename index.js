7bea9d2ac256250ca534d0072c7f7382cd165665
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 15:38:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(badges) {
-  for (let i =0; i<badges.length; i++){
-    console.log("Welcome ${badges[i]}! You are employee #${i+1}." )
+  for (let i =1; i<badges.length; i++){
+    console.log("Welcome ${badges[i]}! You are employee #${i}." )
   }
   return badges
 }
