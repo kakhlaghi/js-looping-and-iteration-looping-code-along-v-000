d882e2c5fef58b6df2d431b1a90b15117496d256
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 15:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function printBadges(badges) {
   for (let i =0; i<badges.length; i++){
     console.log("Welcome ${badges[i]}! You are employee #${i} " )
-    return badges
   }
+  http://159.89.235.135:59474
 }
